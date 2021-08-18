6286ffef3c1c0e052eb45c9e76db68b64ca8a0a1
Volha Kuralenia <Volha_Kuralenia@epam.com>
2021-08-18 21:41:24 +0300

    Add project files.

---
+++ FibonachiType/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciType
+{
+    class Fibonacci
+    {
+        private readonly List<int> FibonacciList;
+        public Fibonacci(int top)
+        {
+            FibonacciList = CreateFibList(top);
+        }
+        private List<int> CreateFibList(int top)
+        {
+            List<int> fibList = new();
+            int a = 0;
+            int b = 1;
+            fibList.Add(a);
+            fibList.Add(b);
+            for (int i = 0; i <= top; i++)
+            {
+                var temp = a;
+                a = b;
+                fibList.Add(b += temp);
+            }
+            return fibList;
+        }
+        public IEnumerator<int> GetEnumerator()
+        {
+            return FibonacciList.GetEnumerator();
+        }
+    }
+    class Program
+    {
+        public static void PrintFibonachiTopHundred(Fibonacci fibList)
+        {
+            Console.WriteLine(" ***** Fibonacci numbers *****");
+            Console.WriteLine($"\tfrom 0 till 100");
+            foreach (var item in fibList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        static void Main(string[] args)
+        {
+            PrintFibonachiTopHundred(new Fibonacci(100));
+        }
+    }
+}
